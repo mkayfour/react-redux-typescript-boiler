fe3565c2b187abc78bd950fe0d149da2b5cd9a1e
Mohit Kumar <mk4227525@gmail.com>
2021-05-17 17:52:17 +0530

    commiting changes

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+  SEARCH_REPOSITORIES = "search_respositories",
+  SEARCH_REPOSITORIES_SUCCESS = "search_respositories_success",
+  SEARCH_REPOSITORIES_ERROR = "search_respositories_error",
+}
---
+++ src/state/reducers/index.ts
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import respositoriesReducer from "./repositoriesReducer";
+
+const reducers = combineReducers({
+  repositories: respositoriesReducer,
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
